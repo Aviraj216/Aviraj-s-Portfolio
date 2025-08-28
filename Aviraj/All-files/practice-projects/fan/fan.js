66749ee32207350a1d9064d0eb72550dbe51064d
Aviraj216 <avirajk216@gmail.com>
2025-08-28 10:19:31 +0000

    This is my complete finished Portfolio Website.

---
+++ Aviraj/All-files/practice-projects/fan/fan.js
@@ -0,0 +1,17 @@
+let a=document.querySelector("img");
+
+function myfunon(){
+    a.style.animationDuration = 3+"s";
+}
+function myfunoff(){
+    a.style.animationDuration = "0s";
+}
+function myfun2(){
+    a.style.animationDuration = "2s";
+}
+function myfun3(){
+    a.style.animationDuration = "1s";
+}
+function myfun4(){
+    a.style.animationDuration = "0.5s";
+}
